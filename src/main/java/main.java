242d53cef23be0de23d67b91554f8304d425b35e
黄致尧 <zhiyao.huang@pcitc.com>
2019-04-24 10:57:42 +0800

    将分支branch1合并到master

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String args[]){
         System.out.println("第一次提交");
         System.out.println("新建分支branch1");
+        System.out.println("将分支branch1合并到master");
     }
 }
